b19d686c9df36daca1e8b8446dcb5c3f80b79950
Konstantyn <k.buhantsev@gmail.com>
2023-04-25 17:35:17 +0300

    getSession

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,31 @@
-import React from "react";
+// import { useSession } from "next-auth/react";
+import { getSession, signIn } from "next-auth/react";
+import React, { useState, useEffect } from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  // const { data: session, status } = useSession();
+  // const loading = status === "loading";
+  // console.log(session, loading);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        signIn();
+      } else {
+        setLoading(false);
+      }
+    };
+    securePage();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return <h1>Dashboard</h1>;
 };
 
 export default Dashboard;
